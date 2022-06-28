20b35d212d4bf890e7a9962ecffad165394b08cb
MellowCo <799478052@qq.com>
2022-06-28 21:38:32 +0800

    feat: isArray

---
+++ src/array.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: licl
+ * @Date: 2022-06-28 21:09:32
+ * @LastEditTime: 2022-06-28 21:09:34
+ * @LastEditors: licl
+ * @Description:
+ */
+export function isArray(obj: any): boolean {
+  return Array.isArray(obj)
+}
