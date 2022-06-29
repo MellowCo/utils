405e0ae7ce2c3bd23c6933058aa4ccaa4641005c
MellowCo <799478052@qq.com>
2022-06-29 21:09:54 +0800

    feat: isString

---
+++ src/string.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: licl
+ * @Date: 2022-06-29 21:07:02
+ * @LastEditTime: 2022-06-29 21:07:03
+ * @LastEditors: licl
+ * @Description:
+ */
+export function isString(arg: any) {
+  return typeof arg === 'string'
+}
