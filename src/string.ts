6145444f0cc199cd4faa0738e53ab13b3e65226c
MellowCo <799478052@qq.com>
2022-07-27 21:28:02 +0800

    feat(string): camelize

--- src/string.ts
+++ src/string.ts
@@ -1,8 +1,18 @@
 /**
  * 是否为string
- * @param {any} arg - 参数
- * @returns {boolean} - 是否为string
+ * @param arg - 参数
+ * @returns 是否为string
  */
 export function isString(arg: any): boolean {
   return typeof arg === 'string'
+}
+
+const camelizeRE = /-(\w)/g
+/**
+ * 驼峰化
+ * @param str - 字符串
+ * @returns 驼峰化后的字符串 user-info => userInfo
+ */
+export const camelize = (str: string): string => {
+  return str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''))
 }
