61707bf2bd8c138375f710ae6045f18b65b57100
MellowCo <799478052@qq.com>
2022-07-27 21:36:13 +0800

    feat(string): hyphenate 大写字母 转为 小写-连接

--- src/string.ts
+++ src/string.ts
@@ -20,7 +20,17 @@ export const camelize = (str: string): string => {
 /**
  * 首字母大写
  * @param str - 字符串
- * @returns 首字母大写后的字符串
+ * @returns 首字母大写后的字符串 userInfo => UserInfo
  */
 export const capitalize
   = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
+
+const hyphenateRE = /\B([A-Z])/g
+/**
+ * 大写字母 转为 小写-连接
+ * @param str - 字符串
+ * @returns UserInfo => user-info
+ */
+export const hyphenate = (str: string) =>
+  str.replace(hyphenateRE, '-$1').toLowerCase()
+
