f7d69e24f86d6d17b29a5255bc16f256658e68cf
MellowCo <799478052@qq.com>
2022-07-27 21:13:25 +0800

    feat(object): hasOwn

--- src/object.ts
+++ src/object.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: licl
  * @Date: 2022-06-28 20:54:33
- * @LastEditTime: 2022-07-12 08:44:36
+ * @LastEditTime: 2022-07-27 21:12:15
  * @LastEditors: licl
  * @Description:
  */
@@ -10,8 +10,8 @@ import { isArray } from './array'
 
 /**
  * 深度拷贝对象
- * @param {object} obj - 对象
- * @returns {object} - 拷贝的对象
+ * @param  obj - 对象
+ * @returns 拷贝的对象
  */
 export function clone(obj: object): object {
   return JSON.parse(JSON.stringify(obj))
@@ -19,8 +19,8 @@ export function clone(obj: object): object {
 
 /**
  * 清除对象中 undefined,null,[]空数组
- * @param {any} obj - 对象
- * @returns {any} - 清除后的对象
+ * @param  obj - 对象
+ * @returns 清除后的对象
  */
 export function clearNull(obj: any): any {
   if (typeof obj === 'object') {
@@ -44,9 +44,20 @@ export function clearNull(obj: any): any {
 
 /**
  * 是否为对象
- * @param {any} arg - 参数
- * @returns {boolean} - 是否为对象
+ * @param arg - 参数
+ * @returns 是否为对象
  */
 export function isObject(arg: any): boolean {
   return typeof arg === 'object'
 }
+
+const hasOwnProperty = Object.prototype.hasOwnProperty
+/**
+ * 是否为对象的属性
+ * @param val - 对象
+ * @param key - 键
+ */
+export const hasOwn = (
+  val: object,
+  key: string | symbol,
+): key is keyof typeof val => hasOwnProperty.call(val, key)
