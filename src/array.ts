f82f9b607b34448b7686a58e64fc1418d0d06d2b
MellowCo <799478052@qq.com>
2022-07-27 21:10:48 +0800

    feat(array): remove el

--- src/array.ts
+++ src/array.ts
@@ -1,8 +1,19 @@
 /**
  * 是否为数组
- * @param {any} obj  - 对象
- * @returns {boolean} - 是否为数组
+ * @param obj  - 对象
+ * @returns 是否为数组
  */
 export function isArray(obj: any): boolean {
   return Array.isArray(obj)
+}
+
+/**
+ * 移除数组中的某个元素
+ * @param arr - 数组
+ * @param el - 元素
+ */
+export const remove = <T>(arr: T[], el: T) => {
+  const i = arr.indexOf(el)
+  if (i > -1)
+    arr.splice(i, 1)
 }
