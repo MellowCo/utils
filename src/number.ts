0eb8741c316eddc29222b0dd1c072c88a5ab0ffa
MellowCo <799478052@qq.com>
2022-07-31 21:40:44 +0800

    feat(number): randomIntegerInRange(生成指定范围的随机整数) randomNumberInRange(生成指定范围的随机小数)

--- src/number.ts
+++ src/number.ts
@@ -7,3 +7,22 @@ export const toNumber = (val: any): any => {
   const n = parseFloat(val)
   return isNaN(n) ? val : n
 }
+/**
+ * 生成指定范围的随机整数
+ * @param min - 最小值
+ * @param max - 最大值
+ * @returns (0,5) => 3
+ */
+export function randomIntegerInRange(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+/**
+ * 生成指定范围的随机小数
+ * @param min - 最小值
+ * @param max - 最大值
+ * @returns (0,5) => 3.0211363285087005
+ */
+export function randomNumberInRange(min: number, max: number) {
+  return Math.random() * (max - min) + min
+}
