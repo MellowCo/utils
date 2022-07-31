dc9144751af010edecd5c192e126c90695e3f6fb
MellowCo <799478052@qq.com>
2022-07-31 22:01:49 +0800

    feat(string): replaceAll 替换所有相同字符串

--- src/string.ts
+++ src/string.ts
@@ -25,3 +25,12 @@ const hyphenateRE = /\B([A-Z])/g
 export const hyphenate = (str: string) =>
   str.replace(hyphenateRE, '-$1').toLowerCase()
 
+/**
+ * 替换所有相同字符串
+ * @param  text - 需要处理的字符串
+ * @param  repstr - 被替换的字符
+ * @param  newstr - 替换后的字符
+ */
+export function replaceAll(text: string, repstr: string, newstr: string) {
+  return text.replace(new RegExp(repstr, 'gm'), newstr)
+}
