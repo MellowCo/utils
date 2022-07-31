6d095074fb88d42e1054bd3511e80ebb4d5a6809
MellowCo <799478052@qq.com>
2022-07-31 21:45:03 +0800

    feat(array): all 布尔全等判断

--- src/array.ts
+++ src/array.ts
@@ -9,3 +9,12 @@ export const remove = <T>(arr: T[], el: T) => {
     arr.splice(i, 1)
 }
 
+/**
+ * 布尔全等判断
+ * @param arr - 数组
+ * @param fn - 判断函数
+ * @returns all([4, 2, 3], x => x > 1) => true
+ */
+export function all(arr: unknown[], fn = Boolean) {
+  return arr.every(fn)
+}
