09f8dd736c81ad8f33ba52872771c111875a49fe
MellowCo <799478052@qq.com>
2022-07-31 21:13:13 +0800

    feat(is): isDate isFunction isSymbol isObject isPromise

--- src/is.ts
+++ src/is.ts
@@ -10,6 +10,17 @@ export const isMap = (val: unknown): val is Map<any, any> =>
 export const isSet = (val: unknown): val is Set<any> =>
   toTypeString(val) === '[object Set]'
 
-export function isString(arg: any): boolean {
-  return typeof arg === 'string'
+export const isString = (val: unknown): val is string => typeof val === 'string'
+
+export const isDate = (val: unknown): val is Date => toTypeString(val) === '[object Date]'
+
+export const isFunction = (val: unknown): val is Function =>
+  typeof val === 'function'
+export const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'
+
+export const isObject = (val: unknown): val is Record<any, any> =>
+  val !== null && typeof val === 'object'
+
+export const isPromise = <T = any>(val: unknown): val is Promise<T> => {
+  return isObject(val) && isFunction(val.then) && isFunction(val.catch)
 }
