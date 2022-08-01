e633b818cd8a3f98f943da04a2e555cbab46f46b
lcl <799478052@qq.com>
2022-08-01 20:05:03 +0800

    feat(misc): params2Url 键值对拼接成URL参数

--- src/misc.ts
+++ src/misc.ts
@@ -96,3 +96,16 @@ export function hexToRGB(hex: string) {
 export function randomBoolean() {
   return Math.random() >= 0.5
 }
+
+/**
+ * 键值对拼接成URL参数
+ * @param obj - 键值对
+ * @returns a=1&b=2
+ */
+export const params2Url = (obj: Object) => {
+  const params = []
+  for (const key in obj)
+    params.push(`${key}=${obj[key]}`)
+
+  return encodeURIComponent(params.join('&'))
+}
