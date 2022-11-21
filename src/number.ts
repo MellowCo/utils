d012576e013506de8da005cf0425fbf61d58f856
MellowCo <799478052@qq.com>
2022-11-21 22:17:22 +0800

    feat(number): update toNumber

--- src/number.ts
+++ src/number.ts
@@ -1,10 +1,12 @@
+import { isString } from '.'
+
 /**
  * 转为数字
  * @param val - 值
  * @returns
  */
 export const toNumber = (val: any): any => {
-  const n = parseFloat(val)
+  const n = isString(val) ? Number(val) : NaN
   return isNaN(n) ? val : n
 }
 
